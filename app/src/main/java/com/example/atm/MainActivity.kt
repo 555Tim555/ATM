4cd368863d4e10421c3f79c6270822a23b11eedf
555Tim555 <s10114105@gmail.com>
2020-07-06 21:00:56 +0800

    initial commit, Login design

---
+++ app/src/main/java/com/example/atm/MainActivity.kt
@@ -0,0 +1,42 @@
+package com.example.atm
+
+import android.app.Activity
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlin.math.log
+
+class MainActivity : AppCompatActivity() {
+    companion object{
+        val RC_LOGIN = 30
+    }
+    var login = false
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        if(!login){
+            Intent(this,LoginActivity::class.java).apply {
+                startActivityForResult(this, RC_LOGIN)
+            }
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if(requestCode == RC_LOGIN){
+            if(resultCode ==Activity.RESULT_OK){
+                val userid = data?.getStringExtra("LOGIN_USERID")
+                val passwd = data?.getStringExtra("LOGIN_PASSWD")
+                Log.d("RESULT","$userid / $passwd")
+            }
+
+        }
+    }
+
+
+}
